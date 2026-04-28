a0fd19575fffe71651ef7cac8b8b833730f11b95
ivan <icarbajal@gmail.com>
2026-04-28 02:05:43 -0600

    Implementacion de limpieza de reservas vencidas

--- service_cine/src/main/java/com/ivancarbajal/service_cine/ServiceCineApplication.java
+++ service_cine/src/main/java/com/ivancarbajal/service_cine/ServiceCineApplication.java
@@ -2,8 +2,10 @@ package com.ivancarbajal.service_cine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ServiceCineApplication {
 
 	public static void main(String[] args) {
--- service_cine/src/main/java/com/ivancarbajal/service_cine/repository/ReservaRepositorio.java
+++ service_cine/src/main/java/com/ivancarbajal/service_cine/repository/ReservaRepositorio.java
@@ -1,5 +1,7 @@
 package com.ivancarbajal.service_cine.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +12,7 @@ import com.ivancarbajal.service_cine.model.Reserva;
 @Repository
 public interface ReservaRepositorio extends JpaRepository<Reserva, Long> {
     Optional<Reserva> findById(Long id);
+
+    List<Reserva> findByEstadoAndFechaReservaBefore(String estado, LocalDateTime tiempoLimite);
+    
 }
